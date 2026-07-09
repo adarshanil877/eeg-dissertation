73f17786c69e860fe6d191941d985588731daa41
Adarsh Anil <adarshanil877@gmail.com>
2026-07-09 22:57:49 +0100

    Tested 95pca didnt work. Back to 90% - Ending day with 55 AUC

--- testing_features.py
+++ testing_features.py
@@ -37,7 +37,7 @@ def preprocess(X_train, X_test):
     X_test = scaler.transform(X_test)
 
     #PCA done with 90% variance
-    pca = PCA(n_components=0.95)
+    pca = PCA(n_components=0.90)
     X_train = pca.fit_transform(X_train)
     X_test = pca.transform(X_test)
 
