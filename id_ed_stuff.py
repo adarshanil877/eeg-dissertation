b05565db01cdeb648b5da7a47199c3fd110ac07f
Adarsh Anil <adarshanil877@gmail.com>
2026-07-31 14:14:38 +0100

    Had variable size issues - trying again with code to handle that.

--- id_ed_stuff.py
+++ id_ed_stuff.py
@@ -262,6 +262,13 @@ def get_shap_top_features(X_train, y_train, feature_names, n_features=10):
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(X_train)
 
+    # Handle different SHAP output formats
+    if isinstance(shap_values, list):
+        shap_values = shap_values[1]
+
+    elif len(shap_values.shape) == 3:
+        shap_values = shap_values[:, :, 1]
+
     #Averaging absolute SHAP value per feature
     mean_abs_shap = np.abs(shap_values).mean(axis=0)
 
